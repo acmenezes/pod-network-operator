301692f1a795d60e7870e9a5d28feef80cee12e3
acmenezes <adcmenezes@gmail.com>
2021-03-20 17:13:55 +0000

    added updateConditions function to primary network controller

--- controllers/podnetwork/primarynetwork_controller.go
+++ controllers/podnetwork/primarynetwork_controller.go
@@ -18,6 +18,7 @@ package podnetwork
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	podnetworkv1alpha1 "github.com/opdev/pod-network-operator/apis/podnetwork/v1alpha1"
@@ -49,34 +50,64 @@ func (r *PrimaryNetworkReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		return ctrl.Result{}, err
 	}
 
-	// Loop through the list of pods with primary newtworks matching labels
-
 	// call finalizer on primary network configuration resource
 
-	// update primary network status
+	// update primary network status condition unknown
 
-	// log new primary network configuration requested
+	condition := podnetworkv1alpha1.Condition{}
 
-	// begin configuration task
+	condition.Type = podnetworkv1alpha1.ConditionTypeUnknown
+	condition.Status = true
+	condition.Reason = "BeginningReconcileFunction"
+	condition.LastHeartbeatTime = ""
+	condition.LastTransitionTime = ""
 
-	// Beginning network configuration task
+	r.PrimaryNetwork.Status.Conditions = append(r.PrimaryNetwork.Status.Conditions, condition)
 
-	// Loop through configuration fields requested
+	err = r.Client.Status().Update(context.TODO(), r.PrimaryNetwork)
+	if err != nil {
+		reqLogger.Error(err, "Couldn't update Primary Network", "PrimaryNetwork", r.PrimaryNetwork.ObjectMeta.Name)
+		return ctrl.Result{}, err
+	}
 
-	// log configuration item in progress
+	// Loop through the list of pods with primary newtworks matching labels
+	podList, err := listPodsWithMatchingLabels("PrimaryNetworkConfiguration", r.PrimaryNetwork.ObjectMeta.Name)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
-	// update status condition InProgress reason item X being configured
+	for _, pod := range podList.Items {
 
-	// call appropriate link configuration function for item passing Pod as parameter
+		// if pod is not in running phase return
+		if pod.Status.Phase != "Running" {
+			fmt.Printf("pod %v phase is %v, requeuing... ", pod.ObjectMeta.Name, pod.Status.Phase)
+			return ctrl.Result{}, nil
+		}
 
-	// check error on return
-	// log error or
-	// log configuration Pod name, field and value - succeeded
+		// update primary network status
 
-	// update status configuration list adding Pod name, field and value configured
+		// log new primary network configuration requested
 
-	// End configuration task
+		// begin configuration task
 
+		// Beginning network configuration task
+
+		// Loop through configuration fields requested
+
+		// log configuration item in progress
+
+		// update status condition InProgress reason item X being configured
+
+		// call appropriate link configuration function for item passing Pod as parameter
+
+		// check error on return
+		// log error or
+		// log configuration Pod name, field and value - succeeded
+
+		// update status configuration list adding Pod name, field and value configured
+
+		// End configuration task
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -85,4 +116,23 @@ func (r *PrimaryNetworkReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&podnetworkv1alpha1.PrimaryNetwork{}).
 		Complete(r)
+}
+
+func (r *PrimaryNetworkReconciler) updatePrimaryNetworkStatusCondition(Type podnetworkv1alpha1.ConditionType, Status bool, Reason string) error {
+
+	condition := podnetworkv1alpha1.Condition{}
+
+	condition.Type = Type
+	condition.Status = true
+	condition.Reason = "BeginningReconcileFunction"
+	condition.LastHeartbeatTime = ""
+	condition.LastTransitionTime = ""
+
+	r.PrimaryNetwork.Status.Conditions = append(r.PrimaryNetwork.Status.Conditions, condition)
+
+	err := r.Client.Status().Update(context.TODO(), r.PrimaryNetwork)
+	if err != nil {
+		return err
+	}
+	return nil
 }
