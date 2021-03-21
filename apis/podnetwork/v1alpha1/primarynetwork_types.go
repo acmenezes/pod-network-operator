6871537e9766d3117a987090246eca547e075e46
acmenezes <adcmenezes@gmail.com>
2021-03-21 01:39:02 +0000

    added message field to Condition Type

--- apis/podnetwork/v1alpha1/primarynetwork_types.go
+++ apis/podnetwork/v1alpha1/primarynetwork_types.go
@@ -43,6 +43,7 @@ type Condition struct {
 	Type               ConditionType `json:"type,omitempty"`
 	Status             bool          `json:"status,omitemtpy"`
 	Reason             string        `json:"reason,omitempty"`
+	Message            string        `json:"message,omitempty"`
 	LastHeartbeatTime  string        `json:"lastHeartbeatTime,omitempty"`
 	LastTransitionTime string        `json:"lastTransitionTime,omitempty"`
 }
